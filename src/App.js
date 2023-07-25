40028aadc81f8e9020780251f731cf85a45f313e
Luís Pinto <Luspin@microsoft.com>
2023-07-25 18:10:52 +0100

    Added Button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
+
+  // Add a click handler to the button
+  const buttonClicked = () => {
+    setCount(count + 1)
+    console.log('Button was clicked! The value of "count" is now: ' + count);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,8 +27,12 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <button id="clickerButton" onClick={buttonClicked} type="button">{`Button clicked: ${count}`}</button>
+        </div>
       </header>
     </div>
+
   );
 }
 
